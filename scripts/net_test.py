f60417af912a7593b411ac69b7f1b2340f38b1f9
Thibault Bronchain <thibault.bronchain@gmail.com>
2016-10-01 13:53:27 +0800

    adding net_test script

---
+++ scripts/net_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''
+Test internet connection
+'''
+
+import requests, os, time
+from requests.exceptions import Timeout
+
+gw_ip='192.168.1.1'
+url_test='https://www.google.com'
+retries=3
+
+try:
+    r = requests.get("http://{0}".format(gw_ip), allow_redirects=False, timeout=10)
+except Exception as e:
+    print("Gateway unreachable")
+    exit(0)
+
+for count in range(0, retries):
+    try:
+        r = requests.get(url_test, allow_redirects=False, timeout=10)
+    except (Timeout) as e:
+        print("Connection timeout, retrying")
+        continue
+    except Exception as e:
+        print("Test uri unreachable, rebooting service")
+        os.system("ssh root@{0} /etc/init.d/redsocks2 stop".format(gw_ip))
+        os.system("ssh root@{0} /etc/init.d/shadosocks stop".format(gw_ip))
+        time.sleep(1)
+        os.system("ssh root@{0} /etc/init.d/shadosocks start".format(gw_ip))
+        os.system("ssh root@{0} /etc/init.d/redsocks2 start".format(gw_ip))
+        break
+    else:
+        print("All good.")
+        break
+
+# EOF
